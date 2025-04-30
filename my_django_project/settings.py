2d63cd7e446d975eda0f3129c91325ed250fa850
Rfrakeradmin <125916766+Rfrakeradmin@users.noreply.github.com>
2025-04-30 08:50:44 -0700

    Sync Fully Working

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_crontab',
     'api',  # Your custom app for OpenSolar integration
 ]
 
